38f1814b6c5b7a500f929d3ddbbda3075f6875cb
ahmed-amo <smart.gaming86@gmail.com>
2025-09-03 12:52:05 +0100

    db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,5 +20,31 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $categories = [
+            [
+                'name' => "Beauty & Fragrance",
+                'slug' => "beauty-fragrance",
+                'image' => "https://images.unsplash.com/photo-1596462502278-27bfdc403348?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+                'color' => "bg-amber-50",
+            ],
+            [
+                'name' => "Health & Personal",
+                'slug' => "health-personal",
+                'image' => "https://images.unsplash.com/photo-1583209814683-c023dd293cc6?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+                'color' => "bg-sky-50",
+            ],
+            [
+                'name' => "Men's Fashion",
+                'slug' => "mens-fashion",
+                'image' => "https://images.unsplash.com/photo-1516257984-b1b4d707412e?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80",
+                'color' => "bg-blue-50",
+            ]
+        ];
+
+        // Actually create the categories in the database
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
     }
 }
